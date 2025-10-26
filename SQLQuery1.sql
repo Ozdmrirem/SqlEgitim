fcf8bf2534369495b927a4fc990484041e475cb0
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-10-26 22:17:17 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,22 @@
+--use SqlEgitimDB
+
+--Create Komutu--
+--create table Kitaplar2(
+--KitapID int not null,
+--KitapAd nvarchar(50) not null,
+--Yazar nvarchar(50) not null,
+--YayinEvi nvarchar(50) not null,
+--Fiyat smallmoney not null
+--)
+
+--Alter Komutu--
+--Alter table Kitaplar2 add SayfaSayisi smallint 
+
+--Sütunun veri tipini deðiþtirmek için kullanýlan komut
+--alter table Kitaplar2 alter column SayfaSayisi int
+
+--Drop Komutu-- tablo silmek icin
+--Drop table Kitaplar2 
+
+--Kitaplar tablosundan sayfa sutununu silmek icin
+--Alter table Kitaplar drop column Sayfa
